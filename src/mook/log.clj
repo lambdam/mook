507589fc63b5d748d7d17b42cabe55b2e490aeb2
Damien Ragoucy <dam@ramdam.io>
2020-05-22 15:39:10 +0200

    Move all code to .cljc files only

--- src/mook/log.clj
+++
@@ -1,8 +0,0 @@
-(ns mook.log)
-
-(def color-log? false)
-
-(defmacro dev-print! [data color]
-  (when color-log?
-    `(js/console.log (str "%c" (-> ~data cljs.pprint/pprint with-out-str))
-                     (str "background: " ~color "; color: white"))))
