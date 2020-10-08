b362d68e90d61a7a94d403320fa09bd9303b598c
Damien Ragoucy <dam@ramdam.io>
2020-10-08 19:41:16 +0200

    Add logging middleware to direct ref example

--- examples/todomvc-direct-ref-mutations/src/todomvc/core.cljs
+++ examples/todomvc-direct-ref-mutations/src/todomvc/core.cljs
@@ -4,6 +4,7 @@
             [mook.react :as r]
             [mook.react :as mr]
             [orchestra-cljs.spec.test :as st]
+            [promesa.core :as p]
             react
             [react-dom :as react-dom]
             [todomvc.components :as c]
@@ -18,12 +19,24 @@
       ::m/state-key ::stores/app-db
       ::m/store*    stores/app-db*}]))
 
+(defn wrap-console-log [command]
+  (fn process-console-log>> [data]
+    (println "Data before\n" data)
+    (-> (command data)
+        (p/then (fn [data']
+                  (println "Data after\n" data')
+                  data')))))
+
 (defn init! []
   (let [out (-> (st/instrument)
                 sort)]
     (js/console.log
       (str "Instrumented functions:\n" (with-out-str (cljs.pprint/pprint out)))))
-  (m/init-mook! {::m/command-middlewares [wrap-ref-state-stores]})
+  (m/init-mook! {::m/command-middlewares [wrap-ref-state-stores
+                                          wrap-console-log
+                                          ;; Add as many middlewares as you wish.
+                                          ;; They will be applied in the declared order.
+                                          ]})
   (react-dom/render
     (mr/create-element c/root)
     (js/document.getElementById "main-app")))
